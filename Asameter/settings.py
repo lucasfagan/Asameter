4fa22ef3c703a0f1cec5928d0905a084e811b267
Lucas Fagan <lucasfagan@verizon.net>
2017-09-25 16:55:20 -0400

    test

--- Asameter/settings.py
+++ Asameter/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,10 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Asameter',
-        'USER': 'postgres'
-        'PASSWORD': 'butt'
-        'HOST': 'localhost'
-        'PORT': '5432'
+        'USER': 'postgres',
+        'PASSWORD': 'butt',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 #DATABASES['default']=dj_database_url.config(conn_max_age=600)
--- albums/models.py
+++ albums/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Album(models.Model):
+    date=models.Date #WHAT IS CORRECT SYNTAX
+    album_title=models.CharField(max_length=200)
+    
+    explicit=models.CharField(max_length=5)
+
+
 # Create your models here.
