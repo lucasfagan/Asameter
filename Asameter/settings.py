be8123084701cb622a517a32b32272645ee47e76
Lucas Fagan <lucasfagan@verizon.net>
2017-09-28 14:03:39 -0400

    testing

--- Asameter/settings.py
+++ Asameter/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Asameter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Asameter',
         'USER': 'postgres',
         'PASSWORD': 'butt',
--- albums/models.py
+++ albums/models.py
@@ -4,9 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Album(models.Model):
-    date=models.Date #WHAT IS CORRECT SYNTAX
+    #date=models.Date #WHAT IS CORRECT SYNTAX
     album_title=models.CharField(max_length=200)
-    
+
     explicit=models.CharField(max_length=5)
 
 
