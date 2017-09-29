7a49511d8ae594feda5ad5050e4a7a96e24ce900
Lucas Fagan <lucasfagan@verizon.net>
2017-09-29 13:33:09 -0400

    connect w database

--- Asameter/settings.py
+++ Asameter/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'albums.apps.AlbumsConfig'
+    'albums.apps.AlbumsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
