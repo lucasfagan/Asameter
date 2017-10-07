531cf294e096e63428f1af2e77611083088d650a
Lucas Fagan <lucasfagan@verizon.net>
2017-10-07 00:19:21 -0400

    design change; implement extended search functions

--- albums/views.py
+++ albums/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import AsameterAlbums
 
 globaldata=AsameterAlbums.objects.all()
-globalsearchterm=""
+globalsearchterms={}
 globalfilter=""
 globalfilterascending=False
 
@@ -24,6 +24,7 @@ def index(request):
         album.artist=album.artist.capitalize()
         album.genre=album.genre.capitalize()
         album.name_no_spaces=album.album.replace(" ","-")
+        album.redscore=int(25.5*album.music_10)
         if album.music_10<=5:
             album.redcolor=int(255)
             album.greencolor=int(album.music_10*51)
@@ -188,50 +189,70 @@ def detail(request, selected_album):
     context['specific_album']=specific_album
     return render(request, 'albums/detail.html', context)
 
+
 def search(request):
     global globaldata
     global globalsearchterm
-    searchmodifier=""
     context={}
     if request.method == "POST":
         globalsearchterm=request.POST.get("searchbar", None)
-        if ":" in globalsearchterm:
-            index=globalsearchterm.index(':')
-            searchmodifier=globalsearchterm[:index]
-            globalsearchterm=globalsearchterm[index+1:]
-    print(searchmodifier.upper())
-    if searchmodifier.upper() in ['ALBUM','GENRE','SCORE','ARTIST']:
-        context['modifier']=searchmodifier.lower().capitalize()
-        if searchmodifier.upper()=='ALBUM':
-            refined_data=AsameterAlbums.objects.filter(album__istartswith=globalsearchterm)
-            refined_data5=AsameterAlbums.objects.filter(album__icontains=globalsearchterm)
-            globaldata=refined_data | refined_data5
-        if searchmodifier.upper()=='GENRE':
-            refined_data3=AsameterAlbums.objects.filter(genre__istartswith=globalsearchterm)
-            refined_data7=AsameterAlbums.objects.filter(genre__icontains=globalsearchterm)
-            globaldata=refined_data3 | refined_data7
-        if searchmodifier.upper()=='SCORE':
-            refined_data4=AsameterAlbums.objects.filter(music_10__istartswith=globalsearchterm)
-            refined_data8=AsameterAlbums.objects.filter(music_10__icontains=globalsearchterm)
-            globaldata=refined_data4 | refined_data8
+    searchdictionary=splitstring(globalsearchterm)
+    #put in here something like if and exists
+    if 'and' in searchdictionary.keys():
+        if searchdictionary['and']=='Y':
+            del searchdictionary['and']
+            updateddata=AsameterAlbums.objects.all()
+            for key in searchdictionary.keys():
+                if key.upper()=='ALBUM':
+                    updateddata=updateddata.filter(album__istartswith=str(searchdictionary[key])) | updateddata.filter(album__icontains=key)
+                elif key.upper()=='GENRE':
+                    updateddata=updateddata.filter(genre__istartswith=str(searchdictionary[key])) | updateddata.filter(genre__icontains=str(searchdictionary[key]))
+                elif key.upper()=='SCORE':
+                    updateddata=updateddata.filter(music_10__istartswith=str(searchdictionary[key])) | updateddata.filter(music_10__icontains=str(searchdictionary[key]))
+                elif key.upper()=='ARTIST':
+                    updateddata=updateddata.filter(artist__istartswith=str(searchdictionary[key])) | updateddata.filter(artist__icontains=str(searchdictionary[key]))
+                else:
+                    data=updateddata.filter(album__istartswith=str(searchdictionary[key]))
+                    data2=updateddata.filter(artist__istartswith=str(searchdictionary[key]))
+                    data3=updateddata.filter(genre__istartswith=str(searchdictionary[key]))
+                    data4=updateddata.filter(music_10__istartswith=str(searchdictionary[key]))
+                    data5=updateddata.filter(album__icontains=str(searchdictionary[key]))
+                    data6=updateddata.filter(artist__icontains=str(searchdictionary[key]))
+                    data7=updateddata.filter(genre__icontains=str(searchdictionary[key]))
+                    data8=updateddata.filter(music_10__icontains=str(searchdictionary[key]))
+                    updateddata=data|data2|data3|data4|data5|data6|data7|data8
+                globaldata=updateddata
         else:
-            refined_data2=AsameterAlbums.objects.filter(artist__istartswith=globalsearchterm)
-            refined_data6=AsameterAlbums.objects.filter(artist__icontains=globalsearchterm)
-            globaldata=refined_data2 | refined_data6
-
+            del searchdictionary['and']
+        #trying to get empty QuerySet (no internet to look at documentation)
+            print(searchdictionary)
+            finaldata=AsameterAlbums.objects.filter(music_10__startswith='fdasdsasds')
+            for key in searchdictionary.keys():
+                if key.upper()=='ALBUM':
+                    #problem w finaldata here
+                    finaldata=finaldata|AsameterAlbums.objects.filter(album__istartswith=str(searchdictionary[key])) | AsameterAlbums.objects.filter(album__icontains=str(searchdictionary[key]))
+                elif key.upper()=='GENRE':
+                    finaldata=finaldata|AsameterAlbums.objects.filter(genre__istartswith=str(searchdictionary[key])) | AsameterAlbums.objects.filter(genre__icontains=str(searchdictionary[key]))
+                elif key.upper()=='SCORE':
+                    finaldata=finaldata|AsameterAlbums.objects.filter(music_10__istartswith=str(searchdictionary[key])) | AsameterAlbums.objects.filter(music_10__icontains=str(searchdictionary[key]))
+                elif key.upper()=='ARTIST':
+                    finaldata=finaldata|AsameterAlbums.objects.filter(artist__istartswith=str(searchdictionary[key])) | AsameterAlbums.objects.filter(artist__icontains=str(searchdictionary[key]))
+                else:
+                    print("no key")
+                    data1=AsameterAlbums.objects.filter(album__istartswith=key)
+                    data2=AsameterAlbums.objects.filter(artist__istartswith=key)
+                    data3=AsameterAlbums.objects.filter(genre__istartswith=key)
+                    data4=AsameterAlbums.objects.filter(music_10__istartswith=key)
+                    data5=AsameterAlbums.objects.filter(album__icontains=key)
+                    data6=AsameterAlbums.objects.filter(artist__icontains=key)
+                    data7=AsameterAlbums.objects.filter(genre__icontains=key)
+                    data8=AsameterAlbums.objects.filter(music_10__icontains=key)
+                    finaldata=data1|data2|data3|data4|data5|data6|data7|data8
+            globaldata=finaldata
+            print(finaldata)
+        context['search_term']=globalsearchterm
     else:
-        refined_data=AsameterAlbums.objects.filter(album__istartswith=globalsearchterm)
-        refined_data2=AsameterAlbums.objects.filter(artist__istartswith=globalsearchterm)
-        refined_data3=AsameterAlbums.objects.filter(genre__istartswith=globalsearchterm)
-        refined_data4=AsameterAlbums.objects.filter(music_10__istartswith=globalsearchterm)
-        refined_data5=AsameterAlbums.objects.filter(album__icontains=globalsearchterm)
-        refined_data6=AsameterAlbums.objects.filter(artist__icontains=globalsearchterm)
-        refined_data7=AsameterAlbums.objects.filter(genre__icontains=globalsearchterm)
-        refined_data8=AsameterAlbums.objects.filter(music_10__icontains=globalsearchterm)
-        globaldata = refined_data | refined_data2 | refined_data3 | refined_data4 | refined_data5 | refined_data6 | refined_data7 | refined_data8
-
-    context['search_term']=globalsearchterm
-
+        globaldata=AsameterAlbums.objects.all()
     for album in globaldata:
         album.album=album.album.capitalize()
         album.artist=album.artist.capitalize()
@@ -243,10 +264,54 @@ def search(request):
         else:
             album.redcolor=int((10-album.music_10)*51)
             album.greencolor=int(255)
-    context['all_albums']=globaldata
+        context['all_albums']=globaldata
+
     return render(request, 'albums/index.html', context)
 
 def main(request):
     context={}
     return render(request, 'albums/main.html', context)
+
+def splitstring(s):
+    #use same modifier multiple times
+    if s=="":
+        return {}
+    searchtermdict={}
+    index1=0
+    index2=0
+    for z in range(0,len(s)):
+        if s[z]=="[":
+            index1=z
+        elif s[z]=="]":
+            index2=z
+    if index2!=0:
+        if s[index1+1:index2].lower()=="and":
+            searchtermdict['and']='Y'
+        else:
+            searchtermdict['and']='N'
+        s=s[0:index1]+s[index2+1:len(s)]
+    else:
+        searchtermdict['and']='N'
+        s=s[0:index1]+s[index2:len(s)]
+    if s[len(s)-1]==" ":
+        s=s[:len(s)-1]
+    lastchar=0
+    for x in range(0,len(s)):
+        if s[x]==";" or x==len(s)-1:
+            if s[x]==";":
+                substring=s[lastchar:x]
+            else:
+                substring=s[lastchar:x+1]
+            if ":" in substring:
+                colonindex=substring.index(":")
+                searchtermdict[substring[:colonindex]]=substring[colonindex+1:]
+            else:
+                searchtermdict[substring]=""
+            if x!=len(s)-1:
+                y=x+1
+                lastchar=y
+                while s[y]==" ":
+                    lastchar=y+1
+                    y+=1
+    return searchtermdict
 # Create your views here.
